6d231f2ba278419e1c60031ffa3e6d0414fc8344
Harsh Chauhan <harshchauhan10may2004@gmail.com>
2025-09-05 16:01:02 +0530

    basic fastapi running

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+app=FastAPI(
+    title="adventure time",
+    description="this is me starting to do fastapi things",
+    version="0.1",
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
 
-if __name__ == "__main__":
-    main()
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_headers=["*"],
+    allow_methods=["*"],
+    
+)
+
+app.get("/")
+def Home():
+    return {message:"Hello world"}
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run("main:app",host="localhost",port=8000,reload=True)
